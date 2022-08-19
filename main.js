fa894ae6f4f831c6e8f534a8edcd34eac4021f09
TheonDelgado <98633607+TheonDelgado@users.noreply.github.com>
2022-08-19 10:20:32 -0400

    step 14

--- main.js
+++ main.js
@@ -60,6 +60,7 @@ nextTwo.onclick = function() {
   nextTwo.hidden = true;
   document.getElementById("word-five").innerHTML = "DEAR";
   document.getElementById("word-six").innerHTML = "FRI-";
+  lastLyric.style.display = "inline-block";
 }
 
 // Write anonymous event handler property and function for the third progress button
